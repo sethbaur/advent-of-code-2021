27c3726dffb3da9943e2b16dab84688d49723aea
Seth Baur <seth.baur@stord.com>
2021-12-06 16:32:37 -0500

    Day 3 part 2

--- lib/days/day_03.ex
+++ lib/days/day_03.ex
@@ -6,6 +6,7 @@ defmodule AdventOfCode.Days.Day03 do
 
   def run(:b) do
     AdventOfCode.load_data("input03.txt")
+    |> get_life_support_rating()
   end
 
   def get_power_consumption(data) do
@@ -49,5 +50,79 @@ defmodule AdventOfCode.Days.Day03 do
       ones > zeros -> "1"
       true -> "0"
     end
+  end
+
+  # part 2
+
+  def get_life_support_rating(data) do
+    get_oxygen_rating(data) * get_scrubber_rating(data)
+  end
+
+  def get_oxygen_rating(data) do
+    rating =
+      data
+      |> Enum.map(fn x -> {x, x} end)
+      |> filter_by_common_digit()
+
+    :erlang.binary_to_integer(rating, 2)
+  end
+
+  def get_scrubber_rating(data) do
+    rating =
+      data
+      |> Enum.map(fn x -> {x, x} end)
+      |> filter_by_least_common_digit()
+
+    :erlang.binary_to_integer(rating, 2)
+  end
+
+  def filter_by_common_digit([{<<_::binary-size(0)>>, orig}]), do: orig
+
+  def filter_by_common_digit(data) do
+    ones =
+      get_ones(data)
+
+    zeros =
+      get_zeros(data)
+
+    cond do
+      length(ones) >= length(zeros) ->
+        filter_by_common_digit(ones)
+
+      true ->
+        filter_by_common_digit(zeros)
+    end
+  end
+
+  def filter_by_least_common_digit([{_, orig}]), do: orig
+
+  def filter_by_least_common_digit(data) do
+    ones =
+      get_ones(data)
+
+    zeros =
+      get_zeros(data)
+
+    cond do
+      length(ones) >= length(zeros) ->
+        filter_by_least_common_digit(zeros)
+
+      true ->
+        filter_by_least_common_digit(ones)
+    end
+  end
+
+  def get_ones(data) do
+    data
+    |> Enum.map(fn {<<head::binary-size(1)>> <> rest, orig} -> {head, {rest, orig}} end)
+    |> Enum.filter(fn {digit, _} -> digit == "1" end)
+    |> Enum.map(fn {_, orig} -> orig end)
+  end
+
+  def get_zeros(data) do
+    data
+    |> Enum.map(fn {<<head::binary-size(1)>> <> rest, orig} -> {head, {rest, orig}} end)
+    |> Enum.filter(fn {digit, _} -> digit == "0" end)
+    |> Enum.map(fn {_, orig} -> orig end)
   end
 end
--- test/days/day_03_test.exs
+++ test/days/day_03_test.exs
@@ -20,4 +20,16 @@ defmodule Day03Test do
   test "multiples gamma rate by epsilon rate" do
     assert 198 = Day03.get_power_consumption(@data)
   end
+
+  test "gets oxygen generator rating" do
+    assert 23 = Day03.get_oxygen_rating(@data)
+  end
+
+  test "gets CO2 scrubber rating" do
+    assert 10 = Day03.get_scrubber_rating(@data)
+  end
+
+  test "gets life support rating" do
+    assert 230 = Day03.get_life_support_rating(@data)
+  end
 end
